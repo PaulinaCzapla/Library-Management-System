577e8176a49e5c4510e7001a960800e2b10e0319
PaulinaCzapla <PaulinaCzapla@users.noreply.github.com>
2020-10-24 21:22:13 +0200

    przebudowa klasy Borrowed
    dodanie nowych metod oraz zmiana atrybutów

--- Projekt/System w bibliotece/System w bibliotece/Borrowed.cpp
+++ Projekt/System w bibliotece/System w bibliotece/Borrowed.cpp
@@ -1,29 +1,91 @@
 #include "Borrowed.h"
 
-template <typename T>
-Borrowed<T>::Borrowed(T data_, Date borrow_date_, Date deadline_)
+
+Borrowed::Borrowed(std::string resource_ID, Date borrow_date_, Date deadline_)
 {
-	this->data = data_;
+	this->resource_ID = resource_ID;
 	this->borrow_date = borrow_date_;
 	this->deadline = deadline_;
 }
 
-template <typename T>
-Borrowed<T>::Borrowed()
+
+Borrowed::Borrowed()
 {
-	this->data;
-	this->borrow_date_;
+	this->resource_ID = "nieznany";
+	this->borrow_date;
 	this->deadline;
 }
 
-template <typename T>
-void Borrowed<T>::display()
-{
-	std::cout << " " << this->data << " wypozyczono: " << this->borrow_date << " termin oddania: " << this->deadline;
-}
-template <typename T>
 
-int Borrowed<T>::count_fine()
+void Borrowed::display(Library* libraries)
+{
+	Text text;
+	switch (this->resource_ID[0])
+	{
+	case 'K':
+		for (int i = 0; i < LIBRARIES_NUM; i++)
+		{
+			auto element = libraries[0].books.get_element(this->resource_ID);
+			if (element)
+			{
+				std::cout << "Typ: ksiazka " << element->get_current_data().get_ID() << " " << element->get_current_data().get_authors_name() << " \"" << element->get_current_data().get_title() << "\" " << element->get_current_data().get_date_string() << std::endl;
+				std::cout << " wypozyczono: " << this->borrow_date << " termin oddania: " << this->deadline;
+				break;
+			}
+		}
+		
+		break;
+	case 'C':
+		for (int i = 0; i < LIBRARIES_NUM; i++)
+		{
+			auto element = libraries[0].cds.get_element(this->resource_ID);
+			if (element)
+			{
+				std::cout << "Typ: plyta CD " << element->get_current_data().get_ID() << " " << element->get_current_data().get_authors_name() << " \"" << element->get_current_data().get_title() << "\" " << element->get_current_data().get_date_string() << std::endl;
+				std::cout << " wypozyczono: " << this->borrow_date << " termin oddania: " << this->deadline;
+				break;
+			}
+		}
+
+		break;
+	case 'F':
+		for (int i = 0; i < LIBRARIES_NUM; i++)
+		{
+			auto element = libraries[0].movies.get_element(this->resource_ID);
+			if (element)
+			{
+				std::cout << "Typ: film " << element->get_current_data().get_ID() << " " << element->get_current_data().get_authors_name() << " \"" << element->get_current_data().get_title() << "\" " << element->get_current_data().get_date_string() << std::endl;
+				std::cout << " wypozyczono: " << this->borrow_date << " termin oddania: " << this->deadline;
+				break;
+			}
+		}
+
+		break;
+	default:
+		text.display_red(" Brak zasobu. ");
+		std::cout << std::endl;
+		break;
+	}
+}
+
+
+
+std::string Borrowed::get_resource_ID()
+{
+	return this->resource_ID;
+}
+
+Date Borrowed::get_borrow_date()
+{
+	return this->borrow_date;
+}
+
+Date Borrowed::get_deadline()
+{
+	return this->deadline;
+}
+
+int Borrowed::count_fine()
 {
 	int diff = this->deadline - this->borrow_date;
 
--- Projekt/System w bibliotece/System w bibliotece/Borrowed.h
+++ Projekt/System w bibliotece/System w bibliotece/Borrowed.h
@@ -1,20 +1,23 @@
 #pragma once
 #include "Date.h"
-
+#include "Library.h"
 
 #define PRICE 7
 
-template<typename T>
+
 class Borrowed
 {
-	T data;
+	std::string resource_ID;
 	Date borrow_date;
 	Date deadline;
 
 public:
-	Borrowed(T, Date, Date);
+	Borrowed(std::string, Date, Date);
 	Borrowed();
-	void display();
+	void display(Library*);
+	std::string get_resource_ID();
+	Date get_borrow_date();
+	Date get_deadline();
 	int count_fine();
 };
 
